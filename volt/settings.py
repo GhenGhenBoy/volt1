b6308fbc04dfe56aed7fac16c9ac7ee3b6fc20fb
GhenGhenBoy <105849903+GhenGhenBoy@users.noreply.github.com>
2022-10-09 04:15:18 +0100

    Update settings.py

--- volt/settings.py
+++ volt/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-@ikodss4!0a%smn0mmxri@4)rc&&a12%pf+*_vfm7wrm40zk_k'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
